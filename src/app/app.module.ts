7a3a796e274ed7cc65401af4c6a166a99f481c94
monu_jangid76 <jangidrohit817@gmail.com>
2023-08-23 19:03:03 +0530

    todo view component add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppComponent } from './app.component';
 import { TodoFormComponent } from './component/todo-form/todo-form.component';
 import { TodoScreenComponent } from './component/todo-screen/todo-screen.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoViewComponent } from './component/todo-screen/todo-view/todo-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoFormComponent,
-    TodoScreenComponent
+    TodoScreenComponent,
+    TodoViewComponent
   ],
   imports: [
     BrowserModule,
