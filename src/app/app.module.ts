2edefb51d7a5889e24f0d9a0f63387982b48f084
monu_jangid76 <jangidrohit817@gmail.com>
2023-08-24 13:47:20 +0530

    remove todo view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,12 @@ import { AppComponent } from './app.component';
 import { TodoFormComponent } from './component/todo-form/todo-form.component';
 import { TodoScreenComponent } from './component/todo-screen/todo-screen.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TodoViewComponent } from './component/todo-screen/todo-view/todo-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoFormComponent,
     TodoScreenComponent,
-    TodoViewComponent
   ],
   imports: [
     BrowserModule,
